5639018026060e9743949052f4c10c77ff62ab9b
Henry Meier <32826270+hcm444@users.noreply.github.com>
2019-06-25 12:56:03 -0700

    Add files via upload

---
+++ Inclassexercise2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inclassexercise2;
+
+/**
+ *
+ * @author henrymeier
+ */
+public class Inclassexercise2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double i = ((9.5*4.5)-(2.5*3))/(45.5-3.5);
+       System.out.print(i);
+        
+    }
+    
+}
